fdedd55b56721f5709da58e66dc5ef537c9fc5bb
ShengKhai <crimsonflare_91@hotmail.com>
2015-02-25 21:05:25 +0800

    Update right_arm.py

--- Modules/Rigging/right_arm.py
+++ Modules/Rigging/right_arm.py
@@ -1,1 +1,5 @@
-
+import maya.cmds as cmds
+ 
+joinlist = (["shoulder_j", [1,10,0]], ["arm_j", [2,10,0]], ["elbow_j", [6,10,0]], ["forearm_j", [10,10,0]])
+for item in joinlist:
+    cmds.joint (n=item[0], p=item[1])
