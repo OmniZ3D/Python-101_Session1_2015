4a6457d7644eb52457ac3b5a2e769cd712233d04
SDR <druilhe.sebastien@gmail.com>
2015-02-28 17:01:21 +0100

    First submittion of rig_arm.py
    First try on rig arm, stll have some issues with the joint orient.

---
+++ Modules/Rigging/rig_arm.py
@@ -0,0 +1,13 @@
+import maya.cmds as cmds
+jointinfo = (['joint_shoulder', [1.0, 5.0,0.0]],['joint_upperarm', [2.0, 5.0, 0.0]],['joint_lowerarm', [4.0, 4.0, -1.0]],['joint_hand', [6.0, 3.0, 0.0]],['temp', [8.0, 2.0, 1.0]])
+print jointinfo
+
+cmds.select(d=True)
+i = 0
+for item in jointinfo:
+        print item
+        j = cmds.joint(n=item[0], p=item[1], rad=0.1)
+        if i > 0:
+            cmds.joint( jointinfo[i-1][0], e=True, zso=True, oj='xyz' )
+        i+=1
+cmds.delete ('temp')
