4cc9ef705ae46c9718e72239209cead224d7dd3a
Keith Osborn <keith@keithosborn.com>
2015-03-02 14:48:06 -0500

    Week 2 Updates
    Slight changes to the code. Added symmetry to the arm joints and button
    added to startup.

--- startup.py
+++ startup.py
@@ -1,15 +1,17 @@
 import maya.cmds as cmds
- 
-cmds.upAxis( ax='y', rv=True )
-cmds.currentUnit( linear='cm' )
-cmds.currentUnit( time='ntsc' )
- 
+
+def runCommand(*args):
+    print args
+    print "Run Command"
+    from Modules.Rigging import rig_arm as right_arm
+    reload(right_arm)
+  
 def createMenu(*args):
     mi = cmds.window('MayaWindow', ma=True, q=True)
     for m in mi:
         if m == 'RDojo_Menu':
             cmds.deleteUI('RDojo_Menu', m=True)
- 
-    cmds.menu('RDojo_Menu', label='RDMenu', to=True, p='MayaWindow')
- 
+  
+    mymenu = cmds.menu('RDojo_Menu', label='RDMenu', to=True, p='MayaWindow')
+    cmds.menuItem(label='Rig Arm', parent=mymenu, command=runCommand)
 createMenu()
