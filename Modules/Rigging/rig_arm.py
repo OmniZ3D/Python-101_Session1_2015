8eaa9a60d897ef4e9b9cf8af7aa4b4ca750ac79b
majobin <majobin12@gmail.com>
2015-03-02 21:54:34 -0500

    rig_arm.py
    changes added to rig_arm.py

--- Modules/Rigging/rig_arm.py
+++ Modules/Rigging/rig_arm.py
@@ -2,9 +2,11 @@ import maya.cmds as cm
 
 ArmJnts = (['jnt_clavicle',[2.0,0.0,0.0]],['jnt_shoulder',[6.0,0.0,0.0]],['jnt_elbow',[12.0,0.0,-2.0]],['jnt_wrist',[18.0,0.0,0.0]])
 
-
-for item in ArmJnts:
-    cm.joint(n=item[0], p = item[1],sym=True)
-    
-cm.select(cm.ls('jnt_*',type='joint'))
-cm.joint(e=True,oj='xyz',sao='xup')
+def createArm():
+    for item in ArmJnts:
+        cm.joint(n=item[0], p = item[1],sym=True)
+        
+    cm.select(cm.ls('jnt_*',type='joint'))
+    cm.joint(e=True,oj='xyz',sao='xup')
+    cm.delete(cn=True,hi=True)
+createArm()
