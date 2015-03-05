d6f6f063242038124a4839a84be96fc1ebf2823f
EvelynSeng <phish.seng@gmail.com>
2015-03-05 22:34:16 +0800

    update - Calling rig_arm in Maya

--- Modules/Rigging/rig_arm.py
+++ Modules/Rigging/rig_arm.py
@@ -5,4 +5,14 @@ armJnt = (['jnt_scapula', [0.25, 10, 0]], ['jnt_shoulder', [2, 10, 0]], ['jnt_el
 
 #create the joints
 for i in armJnt:
-    cmds.joint(n = i[0], p = i[1])
+    #to check if joint exists, we will use a maya command called objExists.
+    #we will also need to use a condition
+    #first, tryout cmds.objExists and print out the return
+    print cmds.objExists(i[0])
+    #Now try using this in a condition.
+    if cmds.objExists(i[0]) == True:
+        #pass can be used to skip part of the script if a condition is not met.
+        pass
+    #if previous conditions are not met, then use 'else' to do something else.
+    else:
+        cmds.joint(n = i[0], p = i[1])
