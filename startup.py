9e86982f2e014a5c9908489b54ac33b59b5d702f
matmatej <m4tm4tej@gmail.com>
2015-03-13 22:22:32 +0100

    Up

--- startup.py
+++ startup.py
@@ -1,15 +1,10 @@
 import maya.cmds as cmds
- 
+  
 cmds.upAxis( ax='y', rv=True )
 cmds.currentUnit( linear='cm' )
 cmds.currentUnit( time='ntsc' )
  
-def createMenu(*args):
-    mi = cmds.window('MayaWindow', ma=True, q=True)
-    for m in mi:
-        if m == 'RDojo_Menu':
-            cmds.deleteUI('RDojo_Menu', m=True)
- 
-    cmds.menu('RDojo_Menu', label='RDMenu', to=True, p='MayaWindow')
- 
-createMenu()
+# Load The UI
+# We havn't talked much about importing modules yet.
+from ui import rdojo_ui as ui
+reload(ui)
