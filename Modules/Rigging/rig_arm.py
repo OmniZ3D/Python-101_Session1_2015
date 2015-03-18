773f2349d7f28777ce1248f8f5145a54fde128d4
danRpaulsen <danrpaulsen@outlook.com>
2015-03-18 00:03:43 -0600

    Catching up with week 3 before moving forward. Doing separate commits to keep track of updates.

--- Modules/Rigging/rig_arm.py
+++ Modules/Rigging/rig_arm.py
@@ -1,94 +1,113 @@
 import maya.cmds as mc
 
 #jointInfo contains the names of the joints to be created
-jointInfo=['arm__1',[5,0,0]],['elbow__1',[10,0,-1]],['wrist__1',[15,0,0]],['wristEnd__1',[18,0,0]]
+jointInfo=['shld__1',[5,0,0]],['elbow__1',[10,0,-1]],['wrist__1',[15,0,0]],['wristEnd__1',[18,0,0]]
 
 #prefix and side of the joint chain
 prefix = 'setup_'
 side = 'l_'
+symmetry = True
 
 #function to create joints
-def createJoints(jointInfo, prefix, side):
+def createJoints(jointInfo, prefix, side, symmetry):
     for each in jointInfo:
 
-        #create joint name from variables
-        jntname = prefix + side + each[0]
-        print 'The joint name is %s ' % each[0]
-
-        #condition that states if joints exist. If true creates a new name
-        if mc.objExists(jntname) == True:
-
-            #split string at defined character
-            print each[0].partition('__')
-            instance = each[0].partition('__')[2]
-
-            #cast instance as a string to add it to another string later
-            newinstance = str(int(instance)+1)
-            print ' Our new instance is %s' % newinstance
-
-            #replace jntname with our instance number
-            jntname = jntname.replace('__'+instance,'__'+newinstance)
-            print ' Our new joint name is %s' % jntname
+        jointlist = []
 
         #lenght of the list
         lstlen = len(jointInfo)
         print ' joinInfo contains this many items %s' % lstlen
 
-        #create joints
-        jnt = mc.joint(p=each[1],n=jntname,sym=True)
-        mc.joint(jnt,e=True,oj='xyz',secondaryAxisOrient='yup',ch=True,zso=True)
 
+        for each in jointInfo:
+            #create joint name from variables
+            jntname = prefix + side + each[0]
 
+            #condition that states if joints exist. If true creates a new name
+            if mc.objExists(jntname) == True:
+                jntname = generateNewJointName(jntname)
 
-        #condition that sees if all joints have been created
-        if each == jointInfo[lstlen-1]:
+            print 'The joint name is %s ' % each[0]
+
+            #create joints
+            jnt = mc.joint(p=each[1],n=jntname,sym=symmetry)
+            print jnt
+
+            #Deselect so we don't make a joint hierarchy
             mc.select(d=True)
 
-            #sends orient joint list, since we have created all the joints
-            allJoints = mc.ls(type='joint')
-            orientJoints(allJoints)
+            if symmetry == True:
+                #Maya Cheat = we know that they have the same name as the original
+                symjnt = renameSymmetryJnt(jnt)
 
-            #function that renames all joints on the right side
-            renameJoints(allJoints)
+                #add the new joint and symmetry joint to the list
+                jointlist.append([jnt, symjnt])
 
-            print "We made all joints + oriented them"
-        else:
-            print "Still making joints here"
+            else:
+                jointlist.append(jnt)
 
-def renameJoints(jnt):
+        #Cycle that will parent the joints
+        for j in range(len(jointlist)):
+            print jointlist[j]
+            if j != 0:
+                mc.parent(jointlist[j][0], jointlist[j-1][0])
+                if symmetry == True:
+                    print(jointlist[j][1], jointlist[j-1][1])
+                    mc.parent(jointlist[j][1], jointlist[j-1][1])
 
-    for each in jnt:
 
-        oldSide = each.partition('_l_')[1]
-        jntIndex = each.partition('__')[2]
+def generateNewJointName(jntname):
+    print "in Gen"
+    print jntname
 
-        #check if it's on the left or right side of the setup
-        if jntIndex == '2':
-            print 'the part of the name that will be replaced is %s'% oldSide
+    #split string at defined character
+    print jntname.partition('__')
+    instance = jntname.partition('__')[2]
 
-            newSide = '_r_'
-            newName = each.replace(oldSide,newSide)
+    #cast instance as a string to add it to another one later
+    newinstance = str(int(instance)+1)
+    print ' Our new instance is %s' % newinstance
 
-            mc.rename(each,newName)
-            print 'the new joint name is %s' % each
+    #replace jntname with instance number
+    jntname = jntname.replace('__'+instance, '__'+newinstance)
+    print ' Our new joint name is %s' % jntname
 
-        '''
-        FIRST ATTEMPT WITH THE listConnections command
+    #return result of function
+    return jntname
 
-        #save connections in a new variable
-        #symConstr = mc.listConnections(connections=True, t='constraint')
+def renameSymmetryJnt(jnt):
+    symConstr = mc.listConnections(jnt, connections=True, t='symmetryConstraint', et=True)
+    print 'constraint'
+    print symConstr
 
-        #replace the string value to run rename command
-        oldJntName = symConstr
-        newJntName = symConstr.replace("_l_","_r_")
+    #condition that checks if the list has items
+    if symConstr != []:
+        for item in symConstr:
+            print item
+            if item.startswith('symmetryConstraint'):
+                con = item
 
-        mc.rename(oldJntName,newJntName)
-        '''
+    if con != None:
+        conlist = mc.listConnections(con, s=False, type='joint')
+        print 'conlist'
+        print conlist
 
+        if conlist[0].startswith(prefix):
+            symjnt = conlist[0]
+            print symjnt
+
+            try:
+                tmpvar = conlist[0].replace('_l_', '_r_')
+                symjntname = tmpvar.replace('__2', '__1')
+                mc.rename(conlist[0], symjntname)
+
+            except: pass
+
+        return (symjntname)
 
 def orientJoints(jnt):
     #cycles through the joint list to orient the joints
     for each in jnt:
         mc.joint(each, e=True, oj='xyz', secondaryAxisOrient='yup', ch=True, zso=True)
 
-createJoints(jointInfo, prefix, side)
+createJoints(jointInfo, prefix, side, symmetry)
