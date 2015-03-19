cecb73443dd9effc03913b2cce6ebda5571d4b70
Ryan Griffin <ryangrif@gmail.com>
2015-03-18 21:46:11 -0400

    Start building Rig_Arm class

--- Modules/Rigging/rig_arm.py
+++ Modules/Rigging/rig_arm.py
@@ -3,3 +3,27 @@ import maya.cmds as mc
 class Rig_Arm:
     def __init__(self, *args):
         print "In Rig Arm"
+
+        """ In the first part of the course we worked out how to build
+        our layout joints.  Now we can move on to generate an arm rig from
+        the layout joints. We will start by outlining the steps for
+        rigging an arm.
+        """
+        """
+        We need a way to identify that a given node in Maya is part
+        of our layout joints.  We could do this by marking any
+        layout joints with an attribute, or we could put the 
+        layout joints in an asset that is marked with a custom attribute.
+        Take a look at the install.py excerpt below to see how we add an 
+        attribute to the layout asset.
+        """
+
+        """
+        Now we need to build our arm rig joints, or we could even use the
+        joints from the layout.  We already have a createJoints function
+        to work with if needed. Most arm rigs have 3 joint chains. (FK, IK, and Rig).
+        The rig joints blend between the ik and fk.
+        """
+        """
+        Create controls for fk joints.
+        """
--- Modules/System/install.py
+++ Modules/System/install.py
@@ -4,34 +4,47 @@ reload(utils)
 import json
 
 def installLayout(layoutfile, side, prefix, symmetry):
-	data = utils.readJson(layoutfile)
-	# Load the data as a dictionary
-	info = json.loads(data)
-	jointInfo = info['Arm']
-	print "Here is the info loaded from the json file"
-	print jointInfo
-	print "Here are the options from the UI"
-	print (side, prefix, symmetry)
+    data = utils.readJson(layoutfile)
+    # Load the data as a dictionary
+    info = json.loads(data)
+    jointInfo = info['Arm']
+    print "Here is the info loaded from the json file"
+    print jointInfo
+    print "Here are the options from the UI"
+    print (side, prefix, symmetry)
 
-	# Now we have all the data we need to build the joints
-	jntinfo = utils.createJoints(jointInfo, prefix, side, symmetry)
+    # Now we have all the data we need to build the joints
+    jntinfo = utils.createJoints(jointInfo, prefix, side, symmetry)
 
-	""" Now I will introduce you to an asset.  An asset is like a 
-	group with more options.  Assets can even hold non-dag nodes.
-	We can put all of our joints and constraints into an asset.
-	If you don't have the symmetry joint option, you can tag
-	the asset as mirrored by adding Mirror to the asset name """
+    """ Now I will introduce you to an asset.  An asset is like a 
+    group with more options.  Assets can even hold non-dag nodes.
+    We can put all of our joints and constraints into an asset.
+    If you don't have the symmetry joint option, you can tag
+    the asset as mirrored by adding Mirror to the asset name """
 
-	assetname = 'Layout_' + side + prefix 
-	lytast = cmds.container(n=assetname)
+    assetname = 'Layout_' + side + prefix 
+    lytast = cmds.container(n=assetname)
+    cmds.addAttr(lytast, shortName="type", longName="type", dt='string', keyable=False)
+    cmds.setAttr(lytast + '.type', prefix, type='string')
 
-	if symmetry == True:
-			mirassetname = 'LayoutMirror_' + side + prefix 
-			mirrorlytast = cmds.container(n=mirassetname)
-	for j in jntinfo:
-		cmds.container(lytast, edit=True, an=j[0])
+    if symmetry == True:
+        mirassetname = 'LayoutMirror_' + side + prefix 
+        mirrorlytast = cmds.container(n=mirassetname)
+        cmds.addAttr(lytast, shortName="type", longName="type", dt='string', keyable=False)
+        cmds.setAttr(lytast + '.type', prefix, type='string')
+        """
+        If you don't have the symmetry joint option,
+        you could add a "mirror attribute here".  
+        In rig arm a mirrored arm rig could be created 
+        if a layout asset has the mirror attribute.
+        """
+    for j in jntinfo:
+        cmds.container(lytast, edit=True, an=j[0])
 
-		# Check for symmetry
-		if symmetry == True:
-			cmds.container(mirrorlytast, edit=True, an=j[1])
+
+        # Check for symmetry
+        if symmetry == True:
+            cmds.container(mirrorlytast, edit=True, an=j[1])
+
+        
 
