56b10c1cde8af9d38754f3d01f09737f0250eacd
rogerm3d <3droger@gmail.com>
2015-03-25 00:19:17 -0700

    joint chain rename function
    add rename function for creating multiple joint chains

--- Modules/Rigging/rig_arm.py
+++ Modules/Rigging/rig_arm.py
@@ -6,28 +6,45 @@ import Modules.System.utils as utils
 class makeJoints:
 
     def __init__(self, jntInfo, part, symmetry, prefix):
-
-        #read joint info for the joints
-
         #set joint attributes
         side='_L'
-        
-        #make the joints
-        self.joints(jntInfo[part], symmetry, side, prefix)
-    def joints(self, jntInfo, symmetry, side, prefix):
+        self.joints(jntInfo, part, symmetry, side, prefix)
+
+        #function to create joints from json library with symmetry and a prefix
+    def joints(self, jntInfo, part, symmetry, side, prefix):
         cmds.select(d=True)
-        for item in jntInfo:
-            cmds.joint(position=item[1], name=prefix + item[0] + side, symmetry=symmetry)
+        jntNum = 0
+        while jntNum < len(jntInfo[part]):
+            jntName = prefix + jntInfo[part][jntNum][0] + side
+                #check for existing joint jntName and make new name if exists
+            if cmds.objExists(jntName) ==True:  
+                self.generateNewJointName(jntInfo, part, jntNum)
+                jntName = prefix + jntInfo[part][jntNum][0] + side
+                #create joint jntName and rename if symmetry == True
+            cmds.joint(position=jntInfo[part][jntNum][1], name=jntName, symmetry=symmetry)
             if symmetry==True:
-                cmds.rename(prefix + item[0] + side + '1' , prefix + item[0] + '_R')
-            cmds.select(d=True)
-        posChild=len(jntInfo)-1
+                cmds.rename(jntName + '1', prefix + jntInfo[part][jntNum][0] + '_R')
+                cmds.select(d=True)
+            jntNum +=1
+            #parent joints into chain
+        posChild=len(jntInfo[part])-1
         posParent=posChild - 1
         while(posChild>=1):
-            cmds.parent(prefix + jntInfo[posChild][0] + side, prefix + jntInfo[posParent][0] + side)
+            cmds.parent(prefix + jntInfo[part][posChild][0] + side, prefix + jntInfo[part][posParent][0] + side)
             if symmetry==True:
-                cmds.parent(prefix + jntInfo[posChild][0] + '_R', prefix + jntInfo[posParent][0] + '_R')
+                cmds.parent(prefix + jntInfo[part][posChild][0] + '_R', prefix + jntInfo[part][posParent][0] + '_R')
             posChild-=1
             posParent-=1
         cmds.select(d=True)
-        cmds.joint (prefix + jntInfo[0][0] + side, e=True, oj='xyz', secondaryAxisOrient='yup', ch=True,zso=True)
+        cmds.joint (prefix + jntInfo[part][0][0] + side, e=True, oj='xyz', secondaryAxisOrient='yup', ch=True,zso=True)
+
+        #create a new name for joints if alreayd exists
+    def generateNewJointName(self, jntInfo, part, jntNum):
+        jntNameLength = len(jntInfo[part][jntNum][0])
+        jntInstant = int(jntInfo[part][jntNum][0][jntNameLength-2:jntNameLength])
+        jntInstant += 1
+        if jntInstant >=10:
+            jntInfo[part][jntNum][0] = jntInfo[part][jntNum][0][0:jntNameLength-2] + str(jntInstant)
+        else:
+            jntInfo[part][jntNum][0] = jntInfo[part][jntNum][0][0:jntNameLength-2] + '0' + str(jntInstant)
+        return jntInfo
